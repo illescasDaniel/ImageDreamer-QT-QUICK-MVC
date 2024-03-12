a2de3a44fd4bad9f776138c7c829f1da04bdbf84
Daniel Illescas Romero <illescas.daniel@protonmail.com>
2024-03-12 20:45:05 +0100

    Refactor build process to delete unused files

--- tasks_utils.py
+++ tasks_utils.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import platform
 from pathlib import Path
 
 
@@ -25,3 +26,34 @@ def get_safetensors_path_or_exception(root_folder: str) -> Path:
 	if safetensors_file is None:
 		raise FileNotFoundError('File not found')
 	return safetensors_file
+
+def delete_unnused_files_for_built_program():
+	files_safe_to_delete: list[str]
+	match platform.system():
+		case "Windows":
+			files_safe_to_delete = __windows_safe_files_to_delete()
+		case "Linux":
+			files_safe_to_delete = __linux_safe_files_to_delete()
+		case _:
+			files_safe_to_delete = []
+	for file in files_safe_to_delete:
+		try:
+			os.remove(file)
+		except Exception as e:
+			print(f'Error removing unnecessary file: {e}')
+			
+def __linux_safe_files_to_delete() -> list[str]:
+	return [
+		'dist/ImageDreamer/_internal/torch/lib/libcudnn_adv_train.so.8',
+		'dist/ImageDreamer/_internal/torch/lib/libcudnn_ops_train.so.8',
+		'dist/ImageDreamer/_internal/torch/lib/libcudnn_cnn_train.so.8',
+		'dist/ImageDreamer/_internal/libcusolver.so.11',
+	]
+
+def __windows_safe_files_to_delete() -> list[str]:
+	return [
+		'dist/ImageDreamer/_internal/torch/lib/cudnn_adv_train64_8.dll',
+		'dist/ImageDreamer/_internal/torch/lib/cudnn_cnn_train64_8.dll',
+		'dist/ImageDreamer/_internal/torch/lib/cudnn_ops_train64_8.dll',
+		'dist/ImageDreamer/_internal/cusolver64_11.dll',
+	]
